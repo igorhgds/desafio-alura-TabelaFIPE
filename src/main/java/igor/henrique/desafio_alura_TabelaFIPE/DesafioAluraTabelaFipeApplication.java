73afec3a9274789b578cffa41490de3916e435f4
Igor Henrique <61789282+igorhgds@users.noreply.github.com>
2025-04-01 17:26:47 -0300

    created class ConsumoAPI

--- src/main/java/igor/henrique/desafio_alura_TabelaFIPE/DesafioAluraTabelaFipeApplication.java
+++ src/main/java/igor/henrique/desafio_alura_TabelaFIPE/DesafioAluraTabelaFipeApplication.java
@@ -1,13 +1,18 @@
 package igor.henrique.desafio_alura_TabelaFIPE;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DesafioAluraTabelaFipeApplication {
+public class DesafioAluraTabelaFipeApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesafioAluraTabelaFipeApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
---
+++ src/main/java/igor/henrique/desafio_alura_TabelaFIPE/service/ConsumoAPI.java
@@ -0,0 +1,23 @@
+package igor.henrique.desafio_alura_TabelaFIPE.service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsumoAPI {
+
+    public String obterDados(String url) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        String json = response.body();
+        return json;
+    }
+}
