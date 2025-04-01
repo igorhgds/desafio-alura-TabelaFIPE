da152daf418aef59133d8fe2bbbc521e2f0dc658
Igor Henrique <61789282+igorhgds@users.noreply.github.com>
2025-04-01 17:18:21 -0300

    created base project

---
+++ src/main/java/igor/henrique/desafio_alura_TabelaFIPE/DesafioAluraTabelaFipeApplication.java
@@ -0,0 +1,13 @@
+package igor.henrique.desafio_alura_TabelaFIPE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioAluraTabelaFipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioAluraTabelaFipeApplication.class, args);
+	}
+
+}
